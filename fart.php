cdd0297e9c5f72d647a1e1d9a3d352ea78a0367e
kevintheepicguy <59523298+kevintheepicguy@users.noreply.github.com>
2022-11-26 07:34:10 -0600

    added login page, logout and inactivity logout

--- fart.php
+++ fart.php
@@ -1,196 +1,240 @@
-<html>
-
-<body>
-    
 <?php
-    $con = mysqli_connect("localhost","kevin","kevin", "beer");
+    // start session to keep track of who can log in
+    session_start();
 
-    // Check connection
-    if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
-    }
-
-    echo "Database connected!". "<br><br><br><br>";
-
-
-    function viewData($con){
-        $sql = "SELECT id, name, price, picture, description from menu;";
-        $result=mysqli_query($con,$sql);
-        
-        if ($result->num_rows > 0) {
-            // output data of each row
-            echo "
-                <table style='padding-left: 5%; width: 95%;'> 
-                    <tr style='border: 1px solid black;'>
-                        <th style='border: 1px solid black;'>ID</th>
-                        <th style='border: 1px solid black;'>Name</th>
-                        <th style='border: 1px solid black;'>Price</th>
-                        <th style='border: 1px solid black;'>Pciture</th>
-                        <th style='border: 1px solid black;'>Description</th>
-                    </tr>";
-            while($row = $result->fetch_assoc()) {
-
-            echo "
-                    <tr style='border: 1px solid black;'>
-                        <td style='border: 1px solid black;'>ID: ". $row["id"]."</td>
-                        <td style='border: 1px solid black;'>name: ". $row["name"]. "</td>
-                        <td style='border: 1px solid black;'>price: " . $row["price"]."</td>
-                        <td style='border: 1px solid black;'>picture:". $row["picture"]."</td>
-                        <td style='border: 1px solid black;'>Description: ". $row["description"]. "</td>
-                    </tr>";
-                }
-                echo "<table>";
-        } else {
-            echo "0 results";
-        }
-        echo "<br><br><br><br>";
-    }
-
-    // if view button pressed view the data 
-    if(array_key_exists('view', $_POST)) {
-
-    viewData($con);
-    }
-
-
-    // if eddit button clicked
-    if(array_key_exists('edit', $_POST)) {
-
-        // i know this is bad, but it works so....
-        $sql = "UPDATE menu SET Name = '";
-        $sql .= $_POST["name"];
-        $sql .= "', price='";
-        $sql .= $_POST["price"];
-        $sql .= "', picture='";
-        $sql .= $_POST["picture"];
-        $sql .= "', description='";
-        $sql .= $_POST["description"];
-        $sql .= "' WHERE id = ";
-        $sql .= $_POST["id"];
-
-        // check if connected to the DB
-        if ($con->query($sql) === TRUE) {
-        echo "New record created successfully";
-        } else {
-        echo "Error: " . $sql . "<br>" . $con->error;
-        }
-
-        // pull and display data from the DB 
-        viewData($con);
-    }
-
-
-
-    // if add button pressed insert the data 
-    if(array_key_exists('add', $_POST)) {
-
-        $data = $_POST["id"].",'".$_POST['name']."',".$_POST["price"].",'".$_POST["picture"]."','".$_POST["description"]."'";
-
-        $sql = "INSERT INTO menu (id, name, price, picture, description)
-        VALUES (". $data. ")";
-
-        // check if connected to the DB
-        if ($con->query($sql) === TRUE) {
-        echo "New record created successfully";
-        } else {
-        echo "Error: " . $sql . "<br>" . $con->error;
-        }
-        
-        // echo the newly added data, this is DEBUG info
-        if (isset($_POST["add"])) {
-            echo "<div> <h2>added data: </h2><h3>";
-            echo "ID: ";
-            echo $_POST["id"];
-            echo "<br>";
-            echo "Name: ";
-            echo $_POST["name"];
-            echo "<br>";
-            echo "Price: ";
-            echo $_POST["price"];
-            echo "<br>";
-            echo "<br>";
-            echo "<br>";
-            echo "</h3></div>";
-        }
-        viewData($con);
-    }
-
-    // if remove button pressed remove the data based on ID
-    if(array_key_exists('remove', $_POST)) {
-
-    $sql = "DELETE FROM `MENU` WHERE `id` = " .$_POST["id"]. ";";
-
-    if ($result=mysqli_query($con,$sql) === TRUE) {
-        echo "removed Item       ID: ".$_POST["id"];
-        echo "<br><br><br>";
-        viewData($con);
-        } else {
-        echo "Error: " . $sql . "<br>" . $con->error;
-        }
+    // debug helper function
+    function debug_to_console($data)
+    {
+        $output = $data;
+        if (is_array($output))
+        $output = implode(',', $output);
+        echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
     }
 ?>
 
-<form method="post">
-    <input style="margin-bottom: 50px;"type="submit" name="view" class="button" value="View data" />  
+<!-- logs user out after 500 amnt of seconds -->
+<meta http-equiv="refresh" content="500;url=logout.php" />
 
-</form>
+<html>
+    <body>
+        <?php
 
-<form method="post">
-    
-    <h2>Edit data </h2>
-    <h3>you can edit any value except the ID. Use ID to select what row you want to change</h3>
-    <p style="display: inline;">id: </p>
-    <input type="number" name="id" placeholder="number's only"/>
+            // if user not logged in redirect to logout page  
+            if ($_SESSION['authenticated'] == False)
+            {
+                header("location: logout.php");
+            }
 
-    <p style="display: inline;">name: </p>
-    <input type="text" name="name" placeholder="character's only "/>
+            // if user is authenticated
+            if (isset($_SESSION['authenticated']) and $_SESSION['authenticated'] == true)
+            {
+                // Database connection
+                $con = mysqli_connect("localhost","kevin","kevin", "beer");
 
-    <p style="display: inline;">price: </p>
-    <input type="number" step="0.01" name="price" placeholder="number or decimal only"/>
-    
-    <p style="display: inline;">Picture (file name)</p>
-    <input type="text" name="picture" placeholder="character's only "/>
+                // Check connection
+                if (mysqli_connect_errno()) 
+                {
+                    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                    exit();
+                }
 
-    <p style="display: inline;">item description</p>
-    <input type="text" name="description" placeholder="character's only "/>
-    
-    <input type="submit" name="edit" class="button" value="Change"/>
-    <br>
-    <br>
-    <br>
-    <br>
-</form>
+                // successful connection to database
+                echo "Database connected!". "<br><br><br><br>";
+
+                // function to display database data in a table
+                function viewData($con)
+                {
+                    // prepair SQL and get result
+                    $sql = "SELECT id, name, price, picture, description from menu;";
+                    $result=mysqli_query($con,$sql);
+                    
+                    // if result echo data into table
+                    if ($result->num_rows > 0 and $_SESSION['authenticated'] == true)
+                    {
+                        // set up top of the table with names of column
+                        echo "
+                            <table style='padding-left: 5%; width: 95%;'> 
+                                <tr style='border: 1px solid black;'>
+                                    <th style='border: 1px solid black;'>ID</th>
+                                    <th style='border: 1px solid black;'>Name</th>
+                                    <th style='border: 1px solid black;'>Price</th>
+                                    <th style='border: 1px solid black;'>Pciture</th>
+                                    <th style='border: 1px solid black;'>Description</th>
+                                </tr>";
+
+                        // fill table with data 
+                        while($row = $result->fetch_assoc()) 
+                        {
+                            echo "
+                                <tr style='border: 1px solid black;'>
+                                    <td style='border: 1px solid black;'>ID: ". $row["id"]."</td>
+                                    <td style='border: 1px solid black;'>name: ". $row["name"]. "</td>
+                                    <td style='border: 1px solid black;'>price: " . $row["price"]."</td>
+                                    <td style='border: 1px solid black;'>picture:". $row["picture"]."</td>
+                                    <td style='border: 1px solid black;'>Description: ". $row["description"]. "</td>
+                                </tr>";
+                        }
+                        echo "<table>";
+                        
+                        //else no results found 
+                        } else 
+                        {
+                            echo "0 results";
+                        }
+
+                    // visual break 
+                    echo "<br><br><br><br>";
+                }
+
+                // if 'view' button pressed and user is authentacted display the data 
+                if(array_key_exists('view', $_POST) and $_SESSION['authenticated'] == true)
+                {           
+                    viewData($con);
+                }
 
 
-<form method="post">
+                // if edit button clicked and user is authentacted
+                if(array_key_exists('edit', $_POST) and $_SESSION['authenticated'] == true) {
+                    
+                    // prepare the SQL string  
+                    // i know this is bad, but it works so....
+                    $sql = "UPDATE menu SET Name = '";
+                    $sql .= $_POST["name"];
+                    $sql .= "', price='";
+                    $sql .= $_POST["price"];
+                    $sql .= "', picture='";
+                    $sql .= $_POST["picture"];
+                    $sql .= "', description='";
+                    $sql .= $_POST["description"];
+                    $sql .= "' WHERE id = ";
+                    $sql .= $_POST["id"];
 
-    <h2>add new data</h2>
-    <p style="display: inline;">id: </p>
-    <input type="number" name="id" placeholder="number's only"/>
+                    // check if connected to the DB and user is authenticated 
+                    if ($con->query($sql) === TRUE and $_SESSION['authenticated'] == true)
+                    {
+                        echo "New record created successfully";
+                    } else {
+                        echo "Error: " . $sql . "<br>" . $con->error;
+                        echo "contact support";
+                    }
 
-    <p style="display: inline;">name: </p>
-    <input type="text" name="name" placeholder="character's only "/>
+                    // display data even if on error to show user what really happened 
+                    viewData($con);
+                }
 
-    <p style="display: inline;">price: </p>
-    <input type="number" step="0.01" name="price" placeholder="number or decimal only"/>
-    
-    <p style="display: inline;">Picture (file name)</p>
-    <input type="text" name="picture" placeholder="character's only "/>
+                // if add button pressed and user authenticated insert the data 
+                if(array_key_exists('add', $_POST) and $_SESSION['authenticated'] == true) {
 
-    <p style="display: inline;">item description</p>
-    <input type="text" name="description" placeholder="character's only "/>
+                    // prepare the SQL for adding data
+                    $data = $_POST["id"].",'".$_POST['name']."',".$_POST["price"].",'".$_POST["picture"]."','".$_POST["description"]."'";
+                    $sql = "INSERT INTO menu (id, name, price, picture, description)
+                    VALUES (". $data. ")";
 
-    <input type="submit" name="add" class="button" value="add"/>
-</form>
+                    // check if connected to the DB
+                    if ($con->query($sql) === TRUE and $_SESSION['authenticated'] == true)
+                    {
+                        echo "New record created successfully";
+                    } else {
+                        echo "Error: " . $sql . "<br>" . $con->error;
+                        echo "contact support";
+                    }
+                    
+                    // show data even on error to show user what the DB looks like
+                    viewData($con);
+                }
 
-<form method="post">
-    <h2 style="margin-bottom: 50px; margin-top: 100px;">remove data</h2>
-    <p style="display: inline;">id: </p>
-    <input type="text" name="id" placeholder="number's only"/>    
-    <input type="submit" name="remove" class="button" value="remove" />
-    </form>
+                // if remove button pressed and user isauthenticated, remove the data based on ID
+                if(array_key_exists('remove', $_POST) and $_SESSION['authenticated'] == true)
+                {
 
-</body>
+                    // prepare SQL to remove based on ID
+                    $sql = "DELETE FROM `MENU` WHERE `id` = " .$_POST["id"]. ";";
 
+                    // if result found and user is authenticated remove data
+                    if ($result=mysqli_query($con,$sql) === TRUE and $_SESSION['authenticated'] == true)
+                    {
+                        echo "removed Item       ID: ".$_POST["id"];
+                        echo "<br><br><br>";
+                    } else 
+                    {
+                        echo "Error: " . $sql . "<br>" . $con->error;
+                        echo "contact support";
+                    }
+
+                    // show data even on error to show user what the DB looks like
+                    viewData($con);
+                }
+
+                // logout button clicked. redirect to logout page 
+                if (array_key_exists('logout', $_POST))
+                {
+                    header("location: logout.php");
+                    die();
+                }
+            } 
+        ?>
+
+        <form method="post">
+        
+            <!-- view data button -->
+            <input style="margin-bottom: 50px;"type="submit" name="view" class="button" value="View data" />  
+            
+            <!-- logout button -->
+            <button name="logout" style="align: right;" onclick="">logout</button>
+        </form>
+
+        
+        <!-- edit data section       DO NOT CHANGE NAME FIELD IT WILL BREAK THE PHP-->
+        <form method="post">
+            <h2>Edit data </h2>
+            <h3>you can edit any value except the ID. Use ID to select what row you want to change</h3>
+            <p style="display: inline;">id: </p>
+            <input type="number" name="id" placeholder="number's only"/>
+
+            <p style="display: inline;">name: </p>
+            <input type="text" name="name" placeholder="character's only "/>
+
+            <p style="display: inline;">price: </p>
+            <input type="number" step="0.01" name="price" placeholder="number or decimal only"/>
+            
+            <p style="display: inline;">upload Picture (WIP)</p>
+            <input type="text" name="picture" placeholder="character's only "/>
+
+            <p style="display: inline;">item description</p>
+            <input type="text" name="description" placeholder="character's only "/>
+            
+            <input type="submit" name="edit" class="button" value="Change"/>
+            <br><br><br><br>
+        </form>
+
+        <!-- add data section       DO NOT CHANGE NAME FIELD IT WILL BREAK THE PHP-->
+        <form method="post">
+            <h2>add new data</h2>
+            <p style="display: inline;">id: </p>
+            <input type="number" name="id" placeholder="number's only"/>
+
+            <p style="display: inline;">name: </p>
+            <input type="text" name="name" placeholder="character's only "/>
+
+            <p style="display: inline;">price: </p>
+            <input type="number" step="0.01" name="price" placeholder="number or decimal only"/>
+            
+            <p style="display: inline;">upload Picture (WIP)</p>
+            <input type="text" name="picture" placeholder="character's only "/>
+
+            <p style="display: inline;">item description</p>
+            <input type="text" name="description" placeholder="character's only "/>
+
+            <input type="submit" name="add" class="button" value="add"/>
+        </form>
+
+        <!-- remove data section       DO NOT CHANGE NAME FIELD IT WILL BREAK THE PHP-->
+        <form method="post">
+            <h2 style="margin-bottom: 50px; margin-top: 100px;">remove data</h2>
+            <p style="display: inline;">id: </p>
+            <input type="text" name="id" placeholder="number's only"/>    
+            <input type="submit" name="remove" class="button" value="remove" />
+        </form>
+        
+    </body>
 </html>
--- login.php
+++ login.php
@@ -1,39 +1,43 @@
 <?php
    include("config.php");
    session_start();
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
+   $_SESSION['authenticated'] = false;
+
+   if($_SERVER["REQUEST_METHOD"] == "POST")
+   {
+      // grab username and password. set authenticated to false
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      
-      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+      $_SESSION['authenticated'] = false;
+    
+      // prepare SQL to search for login credientals 
+      $sql = "SELECT * FROM admin WHERE username = '$myusername' and password = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      
       $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
+      $error = "";
 		
-      if($count == 1) {
-         session_register("myusername");
+      // If match found log user in and set authenticated to true
+      if($count == 1)
+      {
          $_SESSION['login_user'] = $myusername;
-         
-         header("location: welcome.php");
-      }else {
+         $_SESSION['authenticated'] = true;
+         $error = "valid";
+         header("location: fart.php");
+      }else
+      {
          $error = "Your Login Name or Password is invalid";
+         $_SESSION['authenticated'] = false;
+         echo $error;
       }
    }
 ?>
-<html>
-   
+
+<html>   
    <head>
       <title>Login Page</title>
-      
       <style type = "text/css">
-         body {
+         body{
             font-family:Arial, Helvetica, sans-serif;
             font-size:14px;
          }
@@ -46,30 +50,20 @@
             border:#666666 solid 1px;
          }
       </style>
-      
    </head>
    
-   <body bgcolor = "#FFFFFF">
-	
+   <body background-color = "#FFFFFF">
       <div align = "center">
          <div style = "width:300px; border: solid 1px #333333; " align = "left">
             <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
-				
             <div style = "margin:30px">
-               
                <form action = "" method = "post">
                   <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
                   <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
                   <input type = "submit" value = " Submit "/><br />
                </form>
-               
-               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
-					
             </div>
-				
          </div>
-			
       </div>
-
    </body>
 </html>
